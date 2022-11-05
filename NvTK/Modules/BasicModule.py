6e66bf5df17aa9caea783bfb3d3c065e8157eb66
JiaqiLiZju <46318417+JiaqiLiZju@users.noreply.github.com>
2022-11-05 17:08:03 +0800

    module

--- NvTK/Modules/BasicModule.py
+++ NvTK/Modules/BasicModule.py
@@ -129,6 +129,15 @@ class EXP(nn.Module):
         return x.exp()
 
 
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        return self.fn(x, **kwargs) + x
+        
+
 class BasicConv1d(BasicModule):
     """Basic Convolutional Module (1d) in NvTK.
 
